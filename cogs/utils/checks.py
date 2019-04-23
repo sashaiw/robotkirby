26c9bc8c6a9b9ea8cb61546e2780ff76a7c460c9
Sasha <sasha@debian-512mb-nyc3-01.localdomain>
2019-04-23 18:57:25 +0000

    disable whitelist

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -3,19 +3,20 @@ import discord.utils
 import filehandler as fh
 
 def is_licensed_check(message):
-    return message.channel.is_private or message.author.id in fh.read(message.server.id,"licenses")
+    #return message.channel.is_private or message.author.id in fh.read(message.server.id,"licenses")
+    return true
 
 def is_licensed():
     return commands.check(lambda ctx: is_licensed_check(ctx.message))
 
 def is_chron_check(message):
-    return message.author.id == "89398547308380160"
+    return message.author.id in ("345410115563683841", "89398547308380160")
 
 def is_chron():
     return commands.check(lambda ctx: is_chron_check(ctx.message))
 
 def is_admin_check(message):
-    return message.author.id in ("89398547308380160", "89412220898803712") or message.author.id == message.server.owner.id
+    return message.author.id in ("345410115563683841", "89398547308380160", "89412220898803712") or message.author.id == message.server.owner.id
 
 def is_admin():
     return commands.check(lambda ctx: is_admin_check(ctx.message))
